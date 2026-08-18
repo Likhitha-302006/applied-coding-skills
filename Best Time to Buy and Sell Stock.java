69f0491b0a0646ce2c03ea04b0549c51e63c4bf8
Likhitha-302006 <vtu27724@veltech.edu.in>
2026-08-18 14:19:39 +0530

    Add files via upload

---
+++ Best Time to Buy and Sell Stock.java
@@ -0,0 +1,27 @@
+Best Time to Buy and Sell Stock
+class Main {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+
+        int minPrice = prices[0];
+        int profit = 0;
+
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            profit = Math.max(profit, price - minPrice);
+        }
+
+
+        System.out.println(profit);
+    }
+}
+
+Input:
+
+7 1 5 3 6 4
+
+Output:
+
+5
