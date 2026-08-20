67786dfbf43fafefafeb64b3ac5f2e28cb14d479
Likhitha-302006 <vtu27724@veltech.edu.in>
2026-08-20 10:25:39 +0530

    Add files via upload

---
+++ Number of Recent Calls.java
@@ -0,0 +1,25 @@
+Number of Recent Calls
+import java.util.*;
+
+
+class RecentCounter {
+    Queue<Integer> queue;
+
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+
+    public int ping(int t) {
+        queue.offer(t);
+
+
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+
+        return queue.size();
+    }
+}
