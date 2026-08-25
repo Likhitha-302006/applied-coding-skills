af534c3a26f83d15812fba25f61d176d65c7ea91
Likhitha-302006 <vtu27724@veltech.edu.in>
2026-08-25 11:47:21 +0530

    Add files via upload

---
+++ Merge Two Sorted Lists.java
@@ -0,0 +1,65 @@
+class Main {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+
+            current = current.next;
+        }
+
+        if (l1 != null)
+            current.next = l1;
+        else
+            current.next = l2;
+
+        return dummy.next;
+    }
+
+    static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+
+        ListNode result = mergeTwoLists(l1, l2);
+
+        System.out.print("Merged List: ");
+        printList(result);
+    }
+}
+
+Output:
+
+Merged List: 1 2 3 4 5 6
+
